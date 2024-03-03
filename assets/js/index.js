85d207d89f1174d6b2160d6a8960198ff81dd8b2
Diego Coloma <diego.coloma.zarate@gmail.com>
2024-03-03 13:41:49 -0300

    terminado

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+let precioInicial = 400000;
+let cantidadInicial = 0;
+
+let precioSpan = document.querySelector(".precio-inicial");
+let cantidadSpan = document.querySelector(".cantidad");
+let totalSpan = document.querySelector(".valor-total")
+
+precioSpan.innerHTML = precioInicial;
+cantidadSpan.innerHTML = cantidadInicial;
